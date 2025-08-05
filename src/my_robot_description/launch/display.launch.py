2ea70aa5df171ece85d6f384b4c8135442db0b9d
mingtiancai <2427373908@qq.com>
2025-08-06 01:09:08 +0800

    add display rviz2

---
+++ src/my_robot_description/launch/display.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    urdf_path = os.path.join(
+        get_package_share_directory('my_robot_description'),
+        'urdf',
+        'my_robot.urdf'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_path).read()}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
